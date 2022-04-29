ff4d935966f897411f91d6830c9cf8610f99225b
notfound <lsvargas@uc.cl>
2022-04-29 15:52:48 -0400

    chore: small fix

--- src/components/RoutesComponents/ProtectedRoute/index.js
+++ src/components/RoutesComponents/ProtectedRoute/index.js
@@ -4,9 +4,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from '../../../context/authContext';
 
 
-function ProtectedRoute(redirectPath = '/login') {
+function ProtectedRoute({ redirectPath='/login' }) {
   const context = useContext(AuthContext);
 
+  console.log(context)
   if (!context?.user) {
     return <Navigate to={redirectPath} replace />;
   };
